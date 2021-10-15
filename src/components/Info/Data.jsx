588c3cb5e466ba77e6de8a763395d61e0e6ba569
Caio Souza <caio.7s@outlook.com>
2021-10-15 19:51:04 -0300

    feat: Criando Página de Informações

---
+++ src/components/Info/Data.jsx
@@ -0,0 +1,49 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lighttextDesc: true,
+    topLine: 'Gestão Premium',
+    headline: 'Organização dos pedido sob suas ordens',
+    description: 'Tenha controle total dos pedidos do seu negócio, economizando tempo pagando pouco.',
+    buttonLabel: 'Comece Já',
+    imgStart: false,
+    img: '/images/svg-1.svg',
+    alt: 'look orders',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lighttextDesc: false,
+    topLine: 'Pagamentos Flexivel',
+    headline: 'Flexibilidade para o bolso do seu cliente',
+    description: 'Tenha a sua disposição formas de pagamento personalizadas para seu negócio.',
+    buttonLabel: 'Leia Mais',
+    imgStart: true,
+    img: '/images/svg-4.svg',
+    alt: 'credit card',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lighttextDesc: false,
+    topLine: 'Premium App',
+    headline: 'Criar uma conta é extremamente fácil',
+    description: 'Tenha acesso ao nosso aplicativo exclusivo e sem a cobrança de taxas adicionais',
+    buttonLabel: 'Começe Agora',
+    imgStart: false,
+    img: '/images/svg-3.svg',
+    alt: 'cash',
+    primary: false,
+    darkText: true
+};
